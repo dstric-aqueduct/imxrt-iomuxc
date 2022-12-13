31dad00272c053c768c31abae75b9004670f9b5c
djstrickland <96876452+dstric-aqueduct@users.noreply.github.com>
2022-12-13 12:02:27 -0500

    fix to Can macro

--- src/imxrt106x/can.rs
+++ src/imxrt106x/can.rs
@@ -1,4 +1,4 @@
-//! CAN pin implementation
+//! SPI pin implementation
 
 use super::pads::{ad_b0::*, ad_b1::*, b0::*, b1::*, emc::*, sd_b0::*, sd_b1::*};
 
@@ -11,26 +11,27 @@ use crate::{
 //
 // CAN1
 //
-can!(module: U1, alt: 2, pad: GPIO_AD_B1_08, signal: Tx, daisy: None);
-can!(module: U1, alt: 2, pad: GPIO_B0_02, signal: Tx, daisy: None);
-can!(module: U1, alt: 3, pad: GPIO_EMC_17, signal: Tx, daisy: None);
-can!(module: U1, alt: 4, pad: GPIO_SD_B1_02, signal: Tx, daisy: None);
-can!(module: U1, alt: 2, pad: GPIO_AD_B1_09, signal: Rx, daisy: DAISY_CAN1_RX_GPIO_AD_B1_09);
-can!(module: U1, alt: 2, pad: GPIO_B0_03, signal: Rx, daisy: DAISY_CAN1_RX_GPIO_B0_03);
-can!(module: U1, alt: 3, pad: GPIO_EMC_18, signal: Rx, daisy: DAISY_CAN1_RX_GPIO_EMC_18);
-can!(module: U1, alt: 4, pad: GPIO_SD_B1_03, signal: Rx, daisy: DAISY_CAN1_RX_GPIO_SD_B1_03);
+can!(module: U1, alt: 2, pad: AD_B1_08, signal: Tx, daisy: None);
+can!(module: U1, alt: 2, pad: B0_02, signal: Tx, daisy: None);
+can!(module: U1, alt: 3, pad: EMC_17, signal: Tx, daisy: None);
+can!(module: U1, alt: 4, pad: SD_B1_02, signal: Tx, daisy: None);
+can!(module: U1, alt: 2, pad: AD_B1_09, signal: Rx, daisy: Some(DAISY_CAN1_RX_GPIO_AD_B1_09));
+can!(module: U1, alt: 2, pad: B0_03, signal: Rx, daisy: Some(DAISY_CAN1_RX_GPIO_B0_03));
+can!(module: U1, alt: 3, pad: EMC_18, signal: Rx, daisy: Some(DAISY_CAN1_RX_GPIO_EMC_18));
+can!(module: U1, alt: 4, pad: SD_B1_03, signal: Rx, daisy: Some(DAISY_CAN1_RX_GPIO_SD_B1_03));
 
 //
 // CAN2
 //
-can!(module: U2, alt: 0, pad: GPIO_AD_B0_02, signal: Tx, daisy: None);
-can!(module: U2, alt: 3, pad: GPIO_EMC_09, signal: Tx, daisy: None);
-can!(module: U2, alt: 6, pad: GPIO_B1_08, signal: Tx, daisy: None);
-can!(module: U2, alt: 6, pad: GPIO_AD_B0_14, signal: Tx, daisy: None);
-can!(module: U2, alt: 0, pad: GPIO_AD_B0_03, signal: Rx, daisy: DAISY_CAN2_RX_GPIO_AD_B0_03);
-can!(module: U2, alt: 3, pad: GPIO_EMC_10, signal: Rx, daisy: DAISY_CAN2_RX_GPIO_EMC_10);
-can!(module: U2, alt: 6, pad: GPIO_AD_B0_15, signal: Rx, daisy: DAISY_CAN2_RX_GPIO_AD_B0_15);
-can!(module: U2, alt: 6, pad: GPIO_B1_09, signal: Rx, daisy: DAISY_CAN2_RX_GPIO_B1_09);
+can!(module: U2, alt: 0, pad: AD_B0_02, signal: Tx, daisy: None);
+can!(module: U2, alt: 3, pad: EMC_09, signal: Tx, daisy: None);
+can!(module: U2, alt: 6, pad: B1_08, signal: Tx, daisy: None);
+can!(module: U2, alt: 6, pad: AD_B0_14, signal: Tx, daisy: None);
+can!(module: U2, alt: 0, pad: AD_B0_03, signal: Rx, daisy: Some(DAISY_CAN2_RX_GPIO_AD_B0_03));
+can!(module: U2, alt: 3, pad: EMC_10, signal: Rx, daisy: Some(DAISY_CAN2_RX_GPIO_EMC_10));
+can!(module: U2, alt: 6, pad: AD_B0_15, signal: Rx, daisy: Some(DAISY_CAN2_RX_GPIO_AD_B0_15));
+can!(module: U2, alt: 6, pad: B1_09, signal: Rx, daisy: Some(DAISY_CAN2_RX_GPIO_B1_09));
+
 
 
 /// Auto-generated DAISY values
